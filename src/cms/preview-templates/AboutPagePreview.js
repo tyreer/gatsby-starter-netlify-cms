dd8ffa7c683f236eeedcdf841ade93159fa82ab4
Robert Tyrees <robert.tyree@stinkstudios.com>
2019-05-09 12:38:47 +0100

    test env variables

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
+  <>
+  <h1>YOYOYO {process.env.YO}</h1>
   <AboutPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
+  </>
 )
 
 AboutPagePreview.propTypes = {
